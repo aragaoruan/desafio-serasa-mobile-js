3e924f0de8ccbb067c6352d83acccf05be5ec39a
Ruan Aragao <aragao.ruan@gmail.com>
2020-03-10 12:15:22 -0300

    feat: adicionando .env no projeto
    FOi adicionado .env no projeto para facilitar quando for colocar o host da api.

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
+import { API_KEY } from 'react-native-dotenv';
+
 import axios from 'axios';
 
-const host = '192.168.43.7'; // put the host of your machine here
-
 const api = axios.create({
-  baseURL: `http://${host}:3333/`,
+  baseURL: `http://${API_KEY}:3333/`,
 });
 
 export default api;
